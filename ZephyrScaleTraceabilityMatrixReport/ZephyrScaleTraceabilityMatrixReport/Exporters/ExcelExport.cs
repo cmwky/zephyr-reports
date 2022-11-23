46e7cb7915df0df6ea6bb3831e64895bf95f7584
Charles Whitehead <charlesmwhitehead@gmail.com>
2022-11-23 09:48:55 -0500

    removes unused libraries

--- ZephyrScaleTraceabilityMatrixReport/ZephyrScaleTraceabilityMatrixReport/Exporters/ExcelExport.cs
+++ ZephyrScaleTraceabilityMatrixReport/ZephyrScaleTraceabilityMatrixReport/Exporters/ExcelExport.cs
@@ -1,9 +1,4 @@
 ﻿using FastExcel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ZephyrScaleTraceabilityMatrixReport.Models;
 
 namespace ZephyrScaleTraceabilityMatrixReport.Exporters
